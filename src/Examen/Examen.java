0651b8d1ea21ad9aa98ffbcaa95d360c60dde8a8
Usuario <Usuario@DESKTOP-EBETHVO>
2023-05-15 09:14:52 +0200

    Actualizada

--- src/Examen/Examen.java
+++ src/Examen/Examen.java
@@ -4,10 +4,18 @@ public class Examen {
 
 	public static void main(String[] args ) {
 
+		
+		
+		
+		
+		
+		
+		
+		
 		int num=0;
 		
 		System.out.println("El numero " + num + " es par."
-				+ "\nSu resto al dividirlo por dos es 0.");
+				+ "\nSu resto al dividirlo por dos es 0");
 		
 		
 		
