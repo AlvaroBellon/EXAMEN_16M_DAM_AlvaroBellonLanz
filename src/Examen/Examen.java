f39682e33f254d6a7513eabd3d4c647a860eb04a
Usuario <Usuario@DESKTOP-EBETHVO>
2023-05-15 09:05:45 +0200

    Examen primer edicion

---
+++ src/Examen/Examen.java
@@ -0,0 +1,22 @@
+package Examen;
+
+import java.util.Scanner;
+
+public class Examen {
+
+	public static void main(String[] args ) {
+
+		Scanner sc= new Scanner(System.in);
+		int num=0;
+		
+		System.out.println("Buenos días Ceinmark, soy Marta Gómez\r\n"
+				+ "Introduce un número para averiguar si es primo o no:\r\n");
+		
+		num = sc.nextInt();
+		
+		
+		
+		
+		
+	}
+}
